c3ed4f82327cba761f7e570c36965caa40150ea7
Guilherme Bernava <guilhermebernava00@gmail.com>
2021-10-25 16:22:22 -0300

    Funcionando

---
+++ Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExercicioDeFixacaoEnum.Entities
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+
+        public Client() { }
+
+        public Client(string name, string email, DateTime date)
+        {
+            Name = name;
+            Email = email;
+            Date = date;
+        }
+    }
+}
